532ff223b4c6c5f38b2f20d0c505bc1ad29b4420
Alen Kis <akis@tempest.local>
2022-01-21 22:33:16 -0500

    add solutions to chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,15 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi)
+import Data.Int (rem)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = r * r * pi
+
+-- point free madness!
+leftoverCents :: Int -> Int
+leftoverCents = flip rem $ 100
