6e47627111f4459b7bb563f007eb83c340fe9836
Alen Kis <16632804+alenkis@users.noreply.github.com>
2022-01-22 12:16:03 -0500

    add solutions for chapter 4 up to Folds section

--- exercises/chapter4/test/Examples.purs
+++ exercises/chapter4/test/Examples.purs
@@ -31,16 +31,18 @@ length arr =
     1 + (length $ fromMaybe [] $ tail arr)
 
 factors :: Int -> Array (Array Int)
-factors n = filter (\xs -> product xs == n) do
-  i <- 1 .. n
-  j <- i .. n
-  pure [ i, j ]
+factors n =
+  filter (\xs -> product xs == n) do
+    i <- 1 .. n
+    j <- i .. n
+    pure [ i, j ]
 
 factorsV2 :: Int -> Array (Array Int)
-factorsV2 n = filter (\xs -> product xs == n) do
-  i <- 1 .. n
-  j <- i .. n
-  [ [ i, j ] ]
+factorsV2 n =
+  filter (\xs -> product xs == n) do
+    i <- 1 .. n
+    j <- i .. n
+    [ [ i, j ] ]
 
 factorsV3 :: Int -> Array (Array Int)
 factorsV3 n = do
@@ -51,23 +53,27 @@ factorsV3 n = do
 
 factorialTailRec :: Int -> Int -> Int
 factorialTailRec n acc =
-  if n == 0
-    then acc
-    else factorialTailRec (n - 1) (acc * n)
+  if n == 0 then
+    acc
+  else
+    factorialTailRec (n - 1) (acc * n)
 
 lengthTailRec :: forall a. Array a -> Int
 lengthTailRec arr = length' arr 0
   where
   length' :: Array a -> Int -> Int
   length' arr' acc =
-    if null arr'
-      then acc
-      else length' (fromMaybe [] $ tail arr') (acc + 1)
+    if null arr' then
+      acc
+    else
+      length' (fromMaybe [] $ tail arr') (acc + 1)
 
 allFiles :: Path -> Array Path
 allFiles file = file : concatMap allFiles (ls file)
 
 allFiles' :: Path -> Array Path
-allFiles' file = file : do
-  child <- ls file
-  allFiles' child
+allFiles' file =
+  file
+    : do
+        child <- ls file
+        allFiles' child
--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -1,5 +1,64 @@
 module Test.MySolutions where
 
 import Prelude
+import Control.Alternative (guard)
+import Data.Array (concat, cons, filter, head, length, tail, (..))
+import Data.Maybe (fromMaybe)
 
--- Note to reader: Add your solutions to this file
+isEven :: Int -> Boolean
+isEven n = case n `mod` 2 of
+  0 -> true
+  _ -> false
+
+countEven :: Array Int -> Int
+countEven [] = 0
+
+countEven xs = count + (countEven (fromMaybe [] $ tail xs))
+  where
+  count = if isEven (fromMaybe 1 $ head xs) then 1 else 0
+
+squared :: Array Number -> Array Number
+squared xs = (\n -> n * n) <$> xs
+
+keepNonNegative :: Array Number -> Array Number
+keepNonNegative = filter (\n -> n >= 0.0)
+
+infix 8 filter as <$?>
+
+keepNonNegativeRewrite :: Array Number -> Array Number
+keepNonNegativeRewrite xs = (\n -> n >= 0.0) <$?> xs
+
+isPrime :: Int -> Boolean
+isPrime x = length (factors x) == 2
+  where
+  factors n = do
+    i <- 1 .. n
+    j <- 1 .. n
+    guard $ i * j == n
+    pure [ i, j ]
+
+cartesianProduct :: forall a. Array a -> Array a -> Array (Array a)
+cartesianProduct x y = do
+  i <- x
+  j <- y
+  pure [ i, j ]
+
+triples :: Int -> Array (Array Int)
+triples n = do
+  a <- 1 .. n
+  b <- a .. n
+  c <- b .. n
+  guard $ a * a + b * b == c * c
+  pure [ a, b, c ]
+
+primeFactors :: Int -> Array Int
+primeFactors n = factorize 2 n
+  where
+  factorize :: Int -> Int -> Array Int
+  factorize _ 1 = []
+
+  factorize divisor dividend =
+    if dividend `mod` divisor == 0 then
+      cons divisor $ factorize (divisor) (dividend / divisor)
+    else
+      factorize (divisor + 1) dividend
